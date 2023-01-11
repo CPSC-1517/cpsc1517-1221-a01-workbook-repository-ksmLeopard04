5c30222d7e51bc5c6f253abdd5e11a01dd3d860d
ksmLeopard04 <kmangat2@nait.ca>
2023-01-11 14:47:56 -0700

    Public enum and review

---
+++ NhlSolution/NhlSystemClassLibrary/Team.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NhlSystemClassLibrary
+{
+    public class Team
+    {
+        //Name, City, Arena, Conference, Division
+
+        //Greedy constructor
+        public string Name { get; set; }
+    }
+}
