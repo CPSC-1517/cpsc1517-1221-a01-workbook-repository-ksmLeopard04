b5010b77605e5c49892edcdf6421f244a7e14de5
KsmLeopard09 <mangatkuljyot95@gmail.com>
2023-01-16 14:27:35 -0700

    completed team properties

---
+++ NhlSolution/NhlConsoleApp/Program.cs
@@ -0,0 +1,29 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using NhlSystemClassLibrary;
+
+// Prompt and read in the team name
+//Console.Write("Enter the team name: ");
+//string teamName = Console.ReadLine();
+try
+{
+    // Create a new Team instance
+    Team oilers = new Team("Oilers","Edmonton","Rogers Place", Conference.Western, Division.Pacific);
+    // Print the Team Name
+    //Console.WriteLine($"Team Name: {currentTeam.Name}");
+    Console.WriteLine(oilers);
+    //Console.WriteLine($"Name: {oilers.Name}, City: {oilers.City}, Arena: {oilers.Arena}");
+}
+catch(ArgumentNullException ex)
+{
+    Console.WriteLine(ex.Message);  
+}
+catch (ArgumentException ex)
+{
+    Console.WriteLine(ex.Message);
+}
+catch
+{
+    Console.WriteLine("Incorrect exception thrown");
+}
+
---
+++ NhlSolution/NhlSystemClassLibrary/Team.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace NhlSystemClassLibrary
+{
+    public class Team
+    {
+        // Define fully implemented properties with a backing field for: Name, City, Arena
+        private string _name;
+        private string _city;
+        private string _arena;
+
+        public string Name
+        {
+            get
+            {
+                return _name;
+            }
+            set
+            {
+                // Validate new value is not blank and contains only letters a-z
+                if (string.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentNullException(nameof(Name),"Name cannot be blank.");
+                }
+                // Validate new value contains only letters a-z
+                string lettersOnlyPattern = @"^[a-zA-Z ]{1,}$";
+                if (!Regex.IsMatch(value,lettersOnlyPattern))
+                {
+                    throw new ArgumentException("Name cannot only contain letters.");
+                }
+                
+                _name = value.Trim();   // remove leading "   hello" and trailing "hello    " white spaces
+            }
+        }
+
+        public string City
+        {
+            get
+            {
+                return _city;
+            }
+            set
+            {
+                // Verify that new value is not blank
+                if (string.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentNullException(nameof(City), "City cannot be blank.");
+                }
+                // Verify that new value contains 3 or more characters
+                if (value.Trim().Length < 3)
+                {
+                    throw new ArgumentException("City must contain 3 or more characters");
+                }
+                _city = value.Trim();
+            }
+        }
+
+        public string Arena
+        {
+            get
+            {
+                return _arena;
+            }
+            set
+            {
+                // Validate that new value is not blank
+                if (string.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentNullException(nameof(Arena), "Arena value cannot be blank.");
+                }
+                _arena = value.Trim();
+            }
+        }
+        // Define auto-implemented properties for: Conference, Division
+        public Conference Conference { get; set; }
+        public Division Division { get; set; }
+
+
+        // Greedy constructor
+        public Team(string Name, string city, string arena, Conference conference, Division division)
+        {
+            this.Name = Name;
+            City = city;
+            Arena = arena;
+            Conference = conference;
+            Division = division;
+        }
+
+        public override string ToString()
+        {
+            return $"Name: {Name}, City: {City}, Arena: {Arena}, Conference: {Conference}, Division: {Division}";
+        }
+    }
+}
