d76c32cf46d09a50c3bf3a7b595369ce8e23c864
ksmLeopard04 <kmangat2@nait.ca>
2023-03-13 14:50:27 -0600

    SqlStuff in c#

---
+++ WestWindSystem/WestWindSystem/DAL/WestWindContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WestWindSystem.DAL
+{
+    internal class WestWindContext : DbContext
+    {
+        public WestWindContext(DbContextOptions<WestWindContext> options) : base(options)
+        {
+
+        }
+    }
+}
